c2f2830c72d03c2ecddeba16fa0c4f05411214dd
Carlos Barrantes <carlos2004jbs2307@gmail.com>
2024-02-25 09:41:32 -0500

    Proyecto generador-colores terminado

--- generador-colores/src/App.jsx
+++ generador-colores/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Values from "values.js";
+import FormColor from "./components/FormColor";
+import DisplayColors from "./components/DisplayColors";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(new Values("red").all(10));
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Generator Color</h1>
+      <FormColor setList={setList}></FormColor>
+      <DisplayColors list={list}></DisplayColors>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
